b7543c0996cda524aba8674d88e9b517aa2bd193
SzymonGrzybowicz <szymon.grzybowicz@gmail.com>
2019-08-09 21:32:08 +0200

    wykonanie zadania 19.2

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,6 +1,9 @@
 package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -9,28 +12,32 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/task")
 public class TaskController {
+    @Autowired
+    private DbService service;
+    @Autowired
+    private TaskMapper taskMapper;
 
     @RequestMapping(method = RequestMethod.GET, value = "getTasks")
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+        return taskMapper.mapToTaskDtoList(service.getAllTasks());
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getTask/{taskId}")
-    public TaskDto getTask(@PathVariable("taskId") Long taskId){
-        return new TaskDto(1L, "test title", "test_content");
+    public TaskDto getTask(@PathVariable("taskId") Long taskId) {
+        return taskMapper.mapToTaskDto(service.getTaskById(taskId));
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask/{taskId}")
-    public void deleteTask(@PathVariable("taskId") Long taskId){
+    public void deleteTask(@PathVariable("taskId") Long taskId) {
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
     @ResponseBody
-    public TaskDto updateTask(@RequestBody TaskDto taskDto){
+    public TaskDto updateTask(@RequestBody TaskDto taskDto) {
         return new TaskDto(1L, "test title", "test_content");
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "createTask")
-    public void createTask(@RequestBody TaskDto taskDto){
+    public void createTask(@RequestBody TaskDto taskDto) {
     }
 }
---
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -0,0 +1,24 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DbService {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Task getTaskById(Long id) {
+        Optional result = taskRepository.findById(id);
+        return (Task)result.orElse(new Task(-1L, null, null));
+    }
+}
