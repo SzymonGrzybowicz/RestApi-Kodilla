e163c668700441c77e0dd268055a7e667b7bb826
SzymonGrzybowicz <szymon.grzybowicz@gmail.com>
2019-08-09 10:52:35 +0200

    wykonanie zad 18.4

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -1,7 +1,7 @@
 
 package com.crud.tasks;
 
-import com.crud.tasks.domain.TaskDto;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -9,13 +9,6 @@ public class TasksApplication {
 
 
 	public static void main(String[] args) {
-		TaskDto taskDto = new TaskDto(1L, "Test title", "I want to be a coder");
-		Long id = taskDto.getId();
-		String title = taskDto.getTitle();
-		String content = taskDto.getContent();
-
-		System.out.println("id: " + id + "\nTitle: " + title + "\nContent: " + content);
-
-		//SpringApplication.run(TasksApplication.class, args);
+		SpringApplication.run(TasksApplication.class, args);
 	}
 }
---
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -0,0 +1,36 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TaskDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/task")
+public class TaskController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
+    public List<TaskDto> getTasks() {
+        return new ArrayList<>();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getTask/{taskId}")
+    public TaskDto getTask(@PathVariable("taskId") Long taskId){
+        return new TaskDto(1L, "test title", "test_content");
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask/{taskId}")
+    public void deleteTask(@PathVariable("taskId") Long taskId){
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
+    @ResponseBody
+    public TaskDto updateTask(@RequestBody TaskDto taskDto){
+        return new TaskDto(1L, "test title", "test_content");
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "createTask")
+    public void createTask(@RequestBody TaskDto taskDto){
+    }
+}
