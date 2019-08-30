db2357972aa9374551db17e8e8dbd8cb97cc8c59
SzymonGrzybowicz <szymon.grzybowicz@gmail.com>
2019-08-30 19:03:48 +0200

    wykoanie zadania 22.3

---
+++ src/main/java/com/crud/tasks/domain/bagdes/Badges.java
@@ -0,0 +1,20 @@
+package com.crud.tasks.domain.bagdes;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Badges {
+
+    @JsonProperty("votes")
+    private int votes;
+
+    @JsonProperty("attachmentsByType")
+    private AttachmentsByType attachments;
+}
