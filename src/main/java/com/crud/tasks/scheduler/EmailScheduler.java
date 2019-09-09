e331d01a739b547866b2e92deeea3ab71597bc4a
SzymonGrzybowicz <szymon.grzybowicz@gmail.com>
2019-09-09 17:58:46 +0200

    wykonanie zadania 23.4

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -25,10 +25,11 @@ public class EmailScheduler {
     @Scheduled(cron = "0 0 10 * * *")
     public void sentInformationEmail(){
         long size = taskRepository.count();
+        String sentenceEnding = size == 1 ? "task" : "tasks";
         simpleEmailService.send(new Mail(
                 adminConfig.getAdminMail(),
                 SUBJECT,
-                "Currently in database you got: " + size + "tasks",
+                "Currently in database you got: " + size + " " + sentenceEnding,
                 ""
         ));
     }
