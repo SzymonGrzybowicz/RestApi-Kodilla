1f4904f0028f849f3480befd459ae5f75309efd8
SzymonGrzybowicz <szymon.grzybowicz@gmail.com>
2019-09-09 17:01:55 +0200

    wysylanie maila

---
+++ src/main/java/com/crud/tasks/service/TrelloService.java
@@ -0,0 +1,46 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.domain.CreatedTrelloCard;
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TrelloService {
+
+    @Autowired
+    private AdminConfig adminConfig;
+
+    @Autowired
+    private TrelloClient trelloClient;
+
+    @Autowired
+    private SimpleEmailService emailService;
+
+    private static final String SUBJECT = "Tasks: New Trello card";
+
+    public List<TrelloBoardDto> fetchTrelloBoards() {
+        return trelloClient.getTrelloBoards();
+    }
+
+    public CreatedTrelloCard createdTrelloCard(final TrelloCardDto trelloCardDto) {
+        CreatedTrelloCard newCard = trelloClient.createnewCard(trelloCardDto);
+
+        Optional.ofNullable(newCard).ifPresent(
+                card -> emailService.send(new Mail(
+                        adminConfig.getAdminMail(),
+                        SUBJECT,
+                        "New card: " + card.getName() + "has been created on your Trello account",
+                        ""
+        )));
+        return newCard;
+    }
+
+}
